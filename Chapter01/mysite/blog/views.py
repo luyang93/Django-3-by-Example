2500a7db6c5536ef3e5fd00a0374086048322027
luyang93 <510426762@qq.com>
2020-07-31 13:27:16 +0800

    creating forms with Django

--- Chapter01/mysite/blog/views.py
+++ Chapter01/mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+
 from .models import Post
 
 
