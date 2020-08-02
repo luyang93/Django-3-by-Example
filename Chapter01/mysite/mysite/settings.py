732f23e5b113c4a4adaf914a2857638284c81aec
luyang93 <510426762@qq.com>
2020-08-02 11:47:25 +0800

    creating feeds for your blog posts

--- Chapter01/mysite/mysite/settings.py
+++ Chapter01/mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'singleron',
+        'USER': 'singleron',
+        'PASSWORD': 'singleron',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
