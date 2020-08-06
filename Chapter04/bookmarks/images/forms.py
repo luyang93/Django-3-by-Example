47fd11af71a66fb791799b594072246a20bb4fd6
luyang93 <510426762@qq.com>
2020-08-06 13:50:01 +0800

    posting content from other websites

---
+++ Chapter04/bookmarks/images/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+
+from .models import Image
+
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = {'title', 'url', 'description'}
+        widgets = {'url': forms.HiddenInput, }
