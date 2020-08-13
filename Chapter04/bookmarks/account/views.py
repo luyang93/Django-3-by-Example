99e345f3da7244097e9265f6ae7cdb2ab156c398
luyang93 <510426762@qq.com>
2020-08-13 13:30:25 +0800

    adding user actions to the activity stream

--- Chapter04/bookmarks/account/views.py
+++ Chapter04/bookmarks/account/views.py
@@ -1,4 +1,3 @@
-from common.decorators import ajax_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
@@ -7,6 +6,8 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 
+from actions.utils import create_action
+from common.decorators import ajax_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -47,6 +48,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -93,6 +95,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'OK'})
--- Chapter04/bookmarks/images/views.py
+++ Chapter04/bookmarks/images/views.py
@@ -1,4 +1,3 @@
-from common.decorators import ajax_required
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -6,6 +5,8 @@ from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.http import require_POST
 
+from actions.utils import create_action
+from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -23,6 +24,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
@@ -50,6 +52,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'like', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status': 'ok'})
