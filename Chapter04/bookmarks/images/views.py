4e0c71f7864dd3ee35bb0b12dcc2dd6e49355793
luyang93 <510426762@qq.com>
2020-08-14 14:52:56 +0800

    storing item views in Redis

--- Chapter04/bookmarks/images/views.py
+++ Chapter04/bookmarks/images/views.py
@@ -1,3 +1,5 @@
+import redis
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -9,6 +11,9 @@ from actions.utils import create_action
 from common.decorators import ajax_required
 from .forms import ImageCreateForm
 from .models import Image
+
+# connect to redis
+r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
 
 
 @login_required
@@ -38,7 +43,9 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image})
+    # increment total image views by 1
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
 
 @ajax_required
