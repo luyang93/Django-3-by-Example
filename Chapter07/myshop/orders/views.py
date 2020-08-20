729870cccd08a2f828d95dabeba08e4c53c2d019
luyang93 <510426762@qq.com>
2020-08-20 16:30:17 +0800

    integrating the payment gateway

--- Chapter07/myshop/orders/views.py
+++ Chapter07/myshop/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
@@ -18,7 +19,12 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
-            return render(request, 'orders/order/created.html', {'order': order})
+            # set the order in the session
+            request.session['order_id'] = order.id
+            # redirect for payment
+            request.session['order_id'] = order.id
+            # redirect for payment
+            return redirect(reverse(('payment:process')))
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
