91b21b410bba18617294ac01ee72661f62199ab4
luyang93 <510426762@qq.com>
2020-08-26 03:27:38 +0800

    recommending products based on previous purchases

--- Chapter07/myshop/shop/views.py
+++ Chapter07/myshop/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from .recommender import Recommender
 from cart.forms import CartAddProductForm
 from .models import Category, Product
 
@@ -14,7 +14,8 @@ def product_list(request, category_slug=None):
                                      translations__language_code=language,
                                      translations__slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+    return render(request, 'shop/product/list.html',
+                  {'category': category, 'categories': categories, 'products': products})
 
 
 def product_detail(request, id, slug):
@@ -25,4 +26,11 @@ def product_detail(request, id, slug):
                                 translations__slug=slug,
                                 available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product,
+                   'cart_product_form': cart_product_form,
+                   'recommended_products': recommended_products})
