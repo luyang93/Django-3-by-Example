bb9a9681e0043277ba539c02c1e69b19e44f7ee5
luyang93 <510426762@qq.com>
2020-08-31 22:14:23 +0800

    displaying courses

--- Chapter10/educa/courses/urls.py
+++ Chapter10/educa/courses/urls.py
@@ -58,4 +58,19 @@ urlpatterns = [
         views.ContentOrderView.as_view(),
         name='content_order'
     ),
+    path(
+        '',
+        views.CourseListView.as_view(),
+        name='course_list'
+    ),
+    path(
+        'subject/<slug:subject>/',
+        views.CourseListView.as_view(),
+        name='course_list_subject'
+    ),
+    path(
+        '<slug:slug>/',
+        views.CourseDetailView.as_view(),
+        name='course_detail'
+    )
 ]
