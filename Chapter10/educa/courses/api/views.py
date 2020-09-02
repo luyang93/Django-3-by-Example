b10158214f53ff09d5dc42f5634dacc1b25c0f9a
luyang93 <510426762@qq.com>
2020-09-02 17:35:01 +0800

    adding permissions to views

--- Chapter10/educa/courses/api/views.py
+++ Chapter10/educa/courses/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -20,6 +21,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 class CourseEnrollView(APIView):
     authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
