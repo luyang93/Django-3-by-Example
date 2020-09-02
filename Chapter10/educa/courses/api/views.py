84ae01fe14da7eeb6e3d39d57e1aafd32466efe4
luyang93 <510426762@qq.com>
2020-09-02 17:34:21 +0800

    handling authentication

--- Chapter10/educa/courses/api/views.py
+++ Chapter10/educa/courses/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -18,6 +19,7 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
