e4ec592bc2f4d6a71d6c39bd55a5786ab552a713
luyang93 <510426762@qq.com>
2020-09-02 17:55:01 +0800

    serializing course contents

--- Chapter10/educa/courses/api/views.py
+++ Chapter10/educa/courses/api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import SubjectSerializer, CourseSerializer
+from .permissions import IsEnrolled
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
 from ..models import Subject, Course
 
 
@@ -52,3 +53,13 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
                 'enrolled': True
             }
         )
+
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
