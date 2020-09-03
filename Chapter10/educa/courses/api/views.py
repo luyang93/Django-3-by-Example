fd982320cf2732797efa216dece2d2a730761972
luyang93 <510426762@qq.com>
2020-09-03 13:33:58 +0800

    adding additional actions to viewsets

--- Chapter10/educa/courses/api/views.py
+++ Chapter10/educa/courses/api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from .serializers import SubjectSerializer, CourseSerializer
 from ..models import Subject, Course
 
@@ -35,3 +37,18 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated]
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response(
+            {
+                'enrolled': True
+            }
+        )
